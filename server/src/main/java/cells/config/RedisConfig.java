b07876d18f90284d729ba57761a4384b4c83073e
Emmy Steven <hello@emmysteven.com>
2021-05-09 23:13:55 +0100

    add configuration for redis
    fix #15

--- server/src/main/java/cells/config/RedisConfig.java
+++ server/src/main/java/cells/config/RedisConfig.java
@@ -1,15 +1,24 @@
 package cells.config;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-
-@Data
 @Configuration
-@ConfigurationProperties("app.cache")
-public class CacheConfig {
-    public String cacheKey;
-    public Duration expirationDuration;
+public class RedisConfig {
+
+    @Bean
+    RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
 }
