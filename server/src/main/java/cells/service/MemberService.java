2b936cc013460321159b566b000897d65753f9a8
Emmy Steven <hello@emmysteven.com>
2021-05-13 20:58:29 +0100

    refactor: rename methods to be generic

--- server/src/main/java/cells/service/MemberService.java
+++ server/src/main/java/cells/service/MemberService.java
@@ -1,45 +1,54 @@
 package cells.service;
 
 import cells.exception.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Repository;
-import cells.exception.AppException;
 import cells.model.Member;
 import cells.repository.MemberRepository;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Slf4j
+@Service
 public class MemberService {
-    private MemberRepository repository;
+    private final MemberRepository repository;
+    private final RedisTemplate<Long, Member> redisTemplate;
 
-    @Autowired
-    public MemberService(MemberRepository repository) {
+    public MemberService(
+            MemberRepository repository,
+            RedisTemplate<Long, Member> redisTemplate
+    ) {
         this.repository = repository;
+        this.redisTemplate = redisTemplate;
     }
 
-    public List<Member> getAllMembers(){
+    @Cacheable(cacheNames = "members")
+    public List<Member> getAll(Member member){
         return repository.findAll();
     }
 
-    public Member getMemberById(Long id){
+    @Cacheable(cacheNames = "members", key="#id")
+    public Member getById(Long id){
+        log.info("fetching book from db");
         return repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Member", "id", id));
     }
 
-    public Member addMember(Member member) {
+    public Member add(Member member) {
         Optional<Member> studentOptional =
                 repository.findByEmail(member.getEmail());
         if (studentOptional.isPresent()){
             throw new IllegalStateException("email already taken");
         }
+//        redisTemplate.opsForValue().set(member.getId(), member);
         return repository.save(member);
     }
 
-    public void deleteMember(Long id) {
+    public void delete(Long id) {
         boolean exist = repository.existsById(id);
         if (!exist) {
             throw new ResourceNotFoundException("Member", "id", id);
@@ -47,7 +56,7 @@ public class MemberService {
         repository.deleteById(id);
     }
 
-    public Member updateMember(Member member) {
+    public Member update(Member member) {
         return repository.save(member);
     }
 
