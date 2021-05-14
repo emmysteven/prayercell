e75983b9df90e6fbc8459d1e66c89aebffce7ec9
Emmy Steven <hello@emmysteven.com>
2021-05-14 23:22:12 +0100

    perf: add caching properly
    add caching and remove redisTemplate implementation

--- server/src/main/java/cells/service/MemberService.java
+++ server/src/main/java/cells/service/MemberService.java
@@ -4,9 +4,7 @@ import cells.exception.ResourceNotFoundException;
 import cells.model.Member;
 import cells.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,14 +14,9 @@ import java.util.Optional;
 @Service
 public class MemberService {
     private final MemberRepository repository;
-    private final RedisTemplate<Long, Member> redisTemplate;
 
-    public MemberService(
-            MemberRepository repository,
-            RedisTemplate<Long, Member> redisTemplate
-    ) {
+    public MemberService(MemberRepository repository) {
         this.repository = repository;
-        this.redisTemplate = redisTemplate;
     }
 
     @Cacheable(cacheNames = "members")
@@ -39,12 +32,10 @@ public class MemberService {
     }
 
     public Member add(Member member) {
-        Optional<Member> studentOptional =
-                repository.findByEmail(member.getEmail());
+        Optional<Member> studentOptional = repository.findByEmail(member.getEmail());
         if (studentOptional.isPresent()){
             throw new IllegalStateException("email already taken");
         }
-//        redisTemplate.opsForValue().set(member.getId(), member);
         return repository.save(member);
     }
 
