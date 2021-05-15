b0737210050fbe5b51b4d4c4f6b11490da1acfa5
Emmy Steven <hello@emmysteven.com>
2021-05-15 15:50:37 +0100

    perf: make controllers to be managers only
    take out the heavy lifting and add them to UserService

--- server/src/main/java/cells/controller/UserController.java
+++ server/src/main/java/cells/controller/UserController.java
@@ -1,93 +1,35 @@
 package cells.controller;
 
-import cells.model.User;
 import cells.payload.request.LoginRequest;
 import cells.payload.request.SignupRequest;
-import cells.payload.response.ApiResponse;
-import cells.payload.response.JwtAuthResponse;
-import cells.repository.UserRepository;
-import cells.security.JwtUtil;
-import org.springframework.http.HttpStatus;
+import cells.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
 //@CrossOrigin(origins = "http://localhost:4200")
-public class AuthController {
+public class UserController {
 
-    final JwtUtil jwtUtil;
-    final UserRepository userRepository;
-    final PasswordEncoder passwordEncoder;
-    final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
-    public AuthController(
-            JwtUtil jwtUtil,
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder,
-            AuthenticationManager authenticationManager
-    ) {
-        this.jwtUtil = jwtUtil;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsernameOrEmail(),
-                        loginRequest.getPassword()
-                )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = jwtUtil.generateToken(authentication);
-        return ResponseEntity.ok(new JwtAuthResponse(jwt));
+       return userService.authenticate(loginRequest);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return new ResponseEntity(new ApiResponse(false, "Email is already taken!"),
-                    HttpStatus.BAD_REQUEST);
-        }
-
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
-                    HttpStatus.BAD_REQUEST);
-        }
-
-        // Creating user's account
-        User user = new User(
-                signUpRequest.getName(),
-                signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                signUpRequest.getRole(),
-                passwordEncoder.encode(signUpRequest.getPassword())
-        );
-
-        User result = userRepository.save(user);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentContextPath().path("/users/{username}")
-                .buildAndExpand(result.getUsername()).toUri();
-
-        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+        return userService.register(signUpRequest);
     }
+
 }
