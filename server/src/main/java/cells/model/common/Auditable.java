90742a1f87059be5ee76bddb89a729b43b3b69fa
Emmy Steven <hello@emmysteven.com>
2021-05-17 09:28:13 +0100

    perf: removed unneeded imports

--- server/src/main/java/cells/model/common/Auditable.java
+++ server/src/main/java/cells/model/common/Auditable.java
@@ -9,8 +9,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
-import java.io.Serializable;
 import java.time.Instant;
 
 @Data
