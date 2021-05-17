5e61bf484b76c0aae18fe15fbae9dc566a5f37f1
Emmy Steven <hello@emmysteven.com>
2021-05-17 09:31:19 +0100

    style: add comments and removed extra whitespace

--- server/src/main/java/cells/security/CurrentUser.java
+++ server/src/main/java/cells/security/CurrentUser.java
@@ -1,13 +1,15 @@
 package cells.security;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-
 import java.lang.annotation.*;
 
+/**
+ * Custom annotation to access currently authenticated user in the codebase. Works the same as
+ * AuthenticationPrincipal. Created to reduce dependency on Spring Security
+ */
 @Target({ElementType.PARAMETER, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
 @AuthenticationPrincipal
 public @interface CurrentUser {
-
 }
