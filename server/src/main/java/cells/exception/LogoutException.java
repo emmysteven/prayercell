d685c09ddc0613c75538b10d8da4379af691c83b
Emmy Steven <hello@emmysteven.com>
2021-05-19 21:49:42 +0100

    refactor: change class name

--- server/src/main/java/cells/exception/LogoutException.java
+++ server/src/main/java/cells/exception/LogoutException.java
@@ -4,12 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
-public class UserLogoutException extends RuntimeException {
+public class LogoutException extends RuntimeException {
 
     private final String user;
     private final String message;
 
-    public UserLogoutException(String user, String message) {
+    public LogoutException(String user, String message) {
         super(String.format("Couldn't log out device [%s]: [%s])", user, message));
         this.user = user;
         this.message = message;
