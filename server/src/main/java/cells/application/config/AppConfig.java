88d1e27efbfc20a1d93f5c0f6a832138a41fedb1
Emmy Steven <hello@emmysteven.com>
2021-05-21 22:03:05 +0100

    perf: enable auditing

--- server/src/main/java/cells/application/config/AppConfig.java
+++ server/src/main/java/cells/application/config/AppConfig.java
@@ -4,9 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableCaching
+@EnableJpaAuditing
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
