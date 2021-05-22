35b3ea8568ca7ce1bda677f1e201af3f447a3f13
Emmy Steven <hello@emmysteven.com>
2021-05-22 06:12:12 +0100

    add repository for email verification token

---
+++ server/src/main/java/cells/infrastructure/repository/EmailVerificationTokenRepository.java
@@ -0,0 +1,11 @@
+package cells.infrastructure.repository;
+
+import cells.domain.entity.token.EmailVerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
+
+    Optional<EmailVerificationToken> findByToken(String token);
+}
