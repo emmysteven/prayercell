26dcb77cf3068a1c9b7609a1f5a2370ea7aaf38e
Emmy Steven <hello@emmysteven.com>
2021-05-24 21:46:40 +0100

    perf: use CrudRepository in place of JpaRepository

--- server/src/main/java/cells/infrastructure/repository/RoleRepository.java
+++ server/src/main/java/cells/infrastructure/repository/RoleRepository.java
@@ -1,7 +1,8 @@
 package cells.infrastructure.repository;
 
 import cells.domain.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
 }
