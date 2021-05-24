e3f36cb550ba36a2c31ee4e78f04052ca8f5ec3a
Emmy Steven <hello@emmysteven.com>
2021-05-24 21:44:02 +0100

    refactor: add base entity and change column name to lowercase

--- server/src/main/java/cells/domain/entity/PasswordResetToken.java
+++ server/src/main/java/cells/domain/entity/PasswordResetToken.java
@@ -1,42 +1,28 @@
 package cells.domain.entity;
 
-import lombok.*;
+import cells.domain.entity.common.BaseEntity;
+import lombok.Data;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Data
-@Entity(name = "PASSWORD_RESET_TOKEN")
-public class PasswordResetToken {
-
-    @Id
-    @Column(name = "TOKEN_ID")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pwd_reset_token_seq")
-    @SequenceGenerator(name = "pwd_reset_token_seq", allocationSize = 1)
-    private Long id;
+@Entity
+public class PasswordResetToken extends BaseEntity {
 
     @NaturalId
-    @Column(name = "TOKEN_NAME", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String token;
 
-    @Column(name = "EXPIRY_DT", nullable = false)
+    @Column(nullable = false)
     private Instant expiryDate;
 
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "USER_ID")
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
-    public PasswordResetToken(Long id, String token, Instant expiryDate, User user) {
-        this.id = id;
+    public PasswordResetToken(String token, Instant expiryDate, User user) {
         this.token = token;
         this.expiryDate = expiryDate;
         this.user = user;
