5381f0c94ff7da7f050db43f23047ae531f91cae
Emmy Steven <hello@emmysteven.com>
2021-05-25 23:31:13 +0100

    fix: add bean for AuditorAware

--- server/src/main/java/cells/application/config/AppConfig.java
+++ server/src/main/java/cells/application/config/AppConfig.java
@@ -4,12 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableCaching
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AppConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new EntityAuditor();
+    }
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
