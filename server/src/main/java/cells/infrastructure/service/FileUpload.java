facda71c4032367340a659beb5adde56ef18a85e
Emmy Steven <hello@emmysteven.com>
2021-05-26 05:16:00 +0100

    refactor: add appropriate modules

--- server/src/main/java/cells/infrastructure/service/FileUpload.java
+++ server/src/main/java/cells/infrastructure/service/FileUpload.java
@@ -1,4 +1,4 @@
-package cells.infrastructure.services;
+package cells.infrastructure.service;
 
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
