d49c560a849d854c3ff032b1b75cdadc01c00c90
Emmy Steven <hello@emmysteven.com>
2021-05-28 04:12:19 +0100

    fix: first insert into database starts with a random number
    fix #24

--- server/src/main/java/cells/domain/entity/common/BaseEntity.java
+++ server/src/main/java/cells/domain/entity/common/BaseEntity.java
@@ -11,7 +11,7 @@ public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 4044849860361438135L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(updatable = false, nullable = false)
     protected Long id;
 }
