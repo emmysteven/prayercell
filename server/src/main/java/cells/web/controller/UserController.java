8af8bfc6929432e4014f2079a3abc88df98171d7
Emmy Steven <hello@emmysteven.com>
2021-05-30 00:29:59 +0100

    fix: replace swagger2 annotations with springdoc-openapi-ui annotations

--- server/src/main/java/cells/web/controller/UserController.java
+++ server/src/main/java/cells/web/controller/UserController.java
@@ -8,9 +8,9 @@ import cells.domain.entity.CustomUserDetails;
 import cells.infrastructure.security.CurrentUser;
 import cells.infrastructure.service.AuthService;
 import cells.infrastructure.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +21,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
-@Api(value = "User Rest API", description = "Defines endpoints for the logged in user. It's secured by default")
+@Tag(name = "User Rest API", description = "Defines endpoints for the logged in user. It's secured by default")
 
 @Slf4j
 public class UserController {
@@ -44,7 +44,7 @@ public class UserController {
      */
     @GetMapping("/me")
     @PreAuthorize("hasRole('USER')")
-    @ApiOperation(value = "Returns the current user profile")
+    @Operation(summary = "Returns the current user profile")
     public ResponseEntity getUserProfile(@CurrentUser CustomUserDetails currentUser) {
         log.info(currentUser.getEmail() + " has role: " + currentUser.getRoles());
         return ResponseEntity.ok("Hello. This is about me");
@@ -55,7 +55,7 @@ public class UserController {
      */
     @GetMapping("/admins")
     @PreAuthorize("hasRole('ADMIN')")
-    @ApiOperation(value = "Returns the list of configured admins. Requires ADMIN Access")
+    @Operation(summary = "Returns the list of configured admins. Requires ADMIN Access")
     public ResponseEntity getAllAdmins() {
         log.info("Inside secured resource with admin");
         return ResponseEntity.ok("Hello. This is about admins");
@@ -66,11 +66,11 @@ public class UserController {
      */
     @PostMapping("/password/update")
     @PreAuthorize("hasRole('USER')")
-    @ApiOperation(value = "Allows the user to change his password once logged in by supplying the correct current " +
+    @Operation(summary = "Allows the user to change his password once logged in by supplying the correct current " +
             "password")
     public ResponseEntity updateUserPassword(
             @CurrentUser CustomUserDetails customUserDetails,
-            @ApiParam(value = "The UpdatePasswordRequest payload")
+            @Parameter(description = "The UpdatePasswordRequest payload")
             @Valid @RequestBody UpdatePasswordRequest updatePasswordRequest
     ) {
         return authService.updatePassword(customUserDetails, updatePasswordRequest)
@@ -91,7 +91,7 @@ public class UserController {
      * user device.
      */
     @PostMapping("/logout")
-    @ApiOperation(value = "Logs the specified user device and clears the refresh tokens associated with it")
+    @Operation(summary = "Logs the specified user device and clears the refresh tokens associated with it")
     // TODO: Work on this later
     public ResponseEntity logoutUser(@CurrentUser CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(new ApiResponse("Log out successful", true));
