3fba5ab3e779ac5f07bd7c1d92a5de35470666a3
Emmy Steven <hello@emmysteven.com>
2021-05-30 06:42:52 +0100

    refactor: replace configuration for swagger2 with that of springdoc-openapi-ui

--- server/src/main/java/cells/application/config/SwaggerConfig.java
+++ server/src/main/java/cells/application/config/SwaggerConfig.java
@@ -1,34 +1,26 @@
 package cells.application.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("cells"))
-                .paths(PathSelectors.regex("/api.*"))
-                .build()
-                .apiInfo(metaInfo());
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/api/**")
+                .build();
     }
 
-    private ApiInfo metaInfo() {
-        return new ApiInfoBuilder()
-                .description("Backend API For the Auth/User Service")
-                .title("Auth/User API")
-                .version("Unreleased [WIP]")
+    @Bean
+    public GroupedOpenApi authAPI() {
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch("/auth/**")
                 .build();
     }
 
