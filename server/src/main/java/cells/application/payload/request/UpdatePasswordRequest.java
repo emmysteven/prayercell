dae008d61dfe1c283c2e856e3d7cef52097aeff6
Emmy Steven <hello@emmysteven.com>
2021-05-30 06:45:59 +0100

    refactor: replace annotations for swagger2 with that of springdoc-openapi-ui

--- server/src/main/java/cells/application/payload/request/UpdatePasswordRequest.java
+++ server/src/main/java/cells/application/payload/request/UpdatePasswordRequest.java
@@ -1,19 +1,18 @@
 package cells.application.payload.request;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotBlank;
 
-@ApiModel(value = "Update password Request", description = "The update password request payload")
+@Schema(name = "Update password Request", description = "The update password request payload")
 public class UpdatePasswordRequest {
 
     @NotBlank(message = "Old password must not be blank")
-    @ApiModelProperty(value = "Valid current user password", required = true, allowableValues = "NonEmpty String")
+    @Schema(description = "Valid current user password", required = true, allowableValues = "NonEmpty String")
     private String oldPassword;
 
     @NotBlank(message = "New password must not be blank")
-    @ApiModelProperty(value = "Valid new password string", required = true, allowableValues = "NonEmpty String")
+    @Schema(description = "Valid new password string", required = true, allowableValues = "NonEmpty String")
     private String newPassword;
 
     public UpdatePasswordRequest(String oldPassword, String newPassword) {
