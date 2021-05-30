e14bef946aa55650b5345c6b3be8b4e8cf333f51
Emmy Steven <hello@emmysteven.com>
2021-05-30 06:44:28 +0100

    refactor: replace annotations for swagger2 with that of springdoc-openapi-ui

--- server/src/main/java/cells/application/payload/request/LoginRequest.java
+++ server/src/main/java/cells/application/payload/request/LoginRequest.java
@@ -1,22 +1,21 @@
 package cells.application.payload.request;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-@ApiModel(value = "Login Request", description = "The login request payload")
+@Schema(title = "Login Request", description = "The login request payload")
 public class LoginRequest {
 
     @NotBlank(message = "Please fill in your username or email")
-    @ApiModelProperty(value = "Registered username/email", allowableValues = "NonEmpty String", allowEmptyValue = false)
+    @Schema(title = "Registered username/email", allowableValues = "NonEmpty String")
     private String usernameOrEmail;
 
     @NotNull(message = "Login password cannot be blank")
-    @ApiModelProperty(value = "Valid user password", required = true, allowableValues = "NonEmpty String")
+    @Schema(description = "Valid user password", required = true, allowableValues = "NonEmpty String")
     private String password;
 
     public LoginRequest(String usernameOrEmail, String email, String password) {
