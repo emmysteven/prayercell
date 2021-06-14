6b163ef1357454d3324d4fc6e72efca7c9cb2a4f
Emmy Steven <hello@emmysteven.com>
2021-06-15 00:37:22 +0100

    fix: import AuthRoutingModule

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent, FooterComponent } from './components';
-
+import {AuthRoutingModule} from '@app/auth/auth-routing.module'
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FooterComponent,
-    HeaderComponent
-  ],
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, AuthRoutingModule],
+  exports: [FooterComponent, HeaderComponent],
 })
 export class SharedModule { }
