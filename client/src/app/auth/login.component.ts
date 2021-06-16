3ca5e138abdd230bd1e5b124a3c0008c9a7aa1ea
Emmy Steven <hello@emmysteven.com>
2021-06-16 23:14:09 +0100

    refactor: change names of variables

--- client/src/app/auth/login.component.ts
+++ client/src/app/auth/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { first } from "rxjs/operators";
-import { AlertService, AuthService } from "../core/services";
+import { AlertService, AuthService } from "@app/core/services";
 
 @Component({
   selector: 'app-login',
