25702e0c785395d5d4ec02cd0eb9b0bfd26e05bc
Emmy Steven <hello@emmysteven.com>
2021-06-18 22:51:52 +0100

    add http interceptors, app.intialize & home module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "@app/shared";
 import { AuthModule } from '@app/auth';
+import { appInitializer } from '@app/core'
+import { AuthService } from '@app/core/services'
+import { ErrorInterceptor, JwtInterceptor } from '@app/core/interceptors';
+import { HomeModule } from './home/home.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     HttpClientModule,
     AppRoutingModule,
     BrowserModule,
     SharedModule,
-    AuthModule
+    AuthModule,
+    HomeModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AuthService] },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
