80a2a8859277fe080190e0aa62cae650d91d5424
Emmy Steven <hello@emmysteven.com>
2021-06-21 04:33:16 +0100

    fix: add refresh token to request

--- client/src/app/core/app.intialize.ts
+++ client/src/app/core/app.intialize.ts
@@ -1,10 +1,11 @@
 import { AuthService } from '@app/core/services'
 
 export function appInitializer(authService: AuthService) {
+  const user = JSON.parse(localStorage.getItem('user')!);
   return () => new Promise(resolve => {
     // attempt to refresh token on app start up to auto authenticate
     authService
-      .refreshToken()
+      .refreshToken(user.refreshToken)
       .subscribe()
       .add(resolve);
   });
