72ba2e15c8fb1f9be53419a7758d28c8fd385127
Emmy Steven <hello@emmysteven.com>
2021-06-21 04:33:52 +0100

    add fields to user and make all optional

--- client/src/app/core/models/user.ts
+++ client/src/app/core/models/user.ts
@@ -1,10 +1,11 @@
-export class User {
+export class User  {
   id?: string = '';
-  firstname: string = '';
-  lastname: string = '';
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  jwtToken?: string = '';
-  registerAsAdmin: boolean = false;
+  firstname?: string = '';
+  lastname?: string = '';
+  username?: string = '';
+  email?: string = '';
+  password?: string = '';
+  accessToken?: string = '';
+  refreshToken?: string = '';
+  registerAsAdmin?: boolean = false;
 }
