6bb2593f5cbfc4375919ad72cb49e6e21513982f
Emmy Steven <hello@emmysteven.com>
2021-06-22 04:28:21 +0100

    fix: provide an empty fall back object

--- client/src/app/core/app.intialize.ts
+++ client/src/app/core/app.intialize.ts
@@ -1,7 +1,7 @@
 import { AuthService } from '@app/core/services'
 
 export function appInitializer(authService: AuthService) {
-  const user = JSON.parse(localStorage.getItem('user')!);
+  let user = JSON.parse(<string>localStorage.getItem('user') || '{}');
   return () => new Promise(resolve => {
     // attempt to refresh token on app start up to auto authenticate
     authService
