4f5f9fe6a501b79ebe411b36b7d893984b310771
Emmy Steven <hello@emmysteven.com>
2021-06-25 00:00:27 +0100

    add dashboard module

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { CreateComponent } from './member/create/create.component';
+import { ReadComponent } from './member/read/read.component';
+import { UpdateComponent } from './member/update/update.component';
+import { DeleteComponent } from './member/delete/delete.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
