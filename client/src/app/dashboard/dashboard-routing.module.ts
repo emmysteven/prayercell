7758c5d55d67ea2a422415f71d91ff700ac26c4a
Emmy Steven <hello@emmysteven.com>
2021-06-28 04:39:03 +0100

    feat: add routes for components in dashboard module

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from '@app/dashboard/layout.component'
+import { ReadMemberComponent } from '@app/dashboard/member/read-member.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: 'member', component: ReadMemberComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
