75b7baa2473dc700cbba654018bd97f4aae40ff1
Emmy Steven <hello@emmysteven.com>
2021-06-28 04:37:03 +0100

    add components to dashboard module

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { CreateComponent } from './member/create/create.component';
-import { ReadComponent } from './member/read/read.component';
-import { UpdateComponent } from './member/update/update.component';
-import { DeleteComponent } from './member/delete/delete.component';
+import { ReadAdminComponent } from './admin/read-admin.component';
+import { CreateAdminComponent } from './admin/create-admin.component';
+import { UpdateAdminComponent } from './admin/update-admin.component';
+import { DeleteAdminComponent } from './admin/delete-admin.component';
+import { DeleteMemberComponent } from './member/delete-member.component';
+import { UpdateMemberComponent } from './member/update-member.component';
+import { ReadMemberComponent } from './member/read-member.component';
+import { CreateMemberComponent } from './member/create-member.component';
+import { LayoutComponent } from './layout.component';
 
 
 @NgModule({
   declarations: [
-    CreateComponent,
-    ReadComponent,
-    UpdateComponent,
-    DeleteComponent
+    ReadAdminComponent,
+    CreateAdminComponent,
+    UpdateAdminComponent,
+    DeleteAdminComponent,
+    DeleteMemberComponent,
+    UpdateMemberComponent,
+    ReadMemberComponent,
+    CreateMemberComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule,
