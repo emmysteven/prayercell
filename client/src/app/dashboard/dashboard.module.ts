3e4e9da74b0498ff4d363a5abcfa96498f6c8f6f
Emmy Steven <hello@emmysteven.com>
2021-06-30 14:46:13 +0100

    fix: import ReactiveFormsModule

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ReadAdminComponent } from './admin/read-admin.component';
 import { CreateAdminComponent } from './admin/create-admin.component';
 import { UpdateAdminComponent } from './admin/update-admin.component';
 import { DeleteAdminComponent } from './admin/delete-admin.component';
-import { DeleteMemberComponent } from './member/delete-member.component';
-import { UpdateMemberComponent } from './member/update-member.component';
-import { ReadMemberComponent } from './member/read-member.component';
-import { CreateMemberComponent } from './member/create-member.component';
+import { DeleteMemberComponent } from '@app/dashboard/member';
+import { UpdateMemberComponent } from '@app/dashboard/member';
+import { ListMemberComponent } from '@app/dashboard/member';
+import { addMemberComponent } from '@app/dashboard/member';
 import { LayoutComponent } from './layout.component';
 
 
@@ -21,12 +22,13 @@ import { LayoutComponent } from './layout.component';
     DeleteAdminComponent,
     DeleteMemberComponent,
     UpdateMemberComponent,
-    ReadMemberComponent,
-    CreateMemberComponent,
+    ListMemberComponent,
+    addMemberComponent,
     LayoutComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     DashboardRoutingModule
   ]
 })
