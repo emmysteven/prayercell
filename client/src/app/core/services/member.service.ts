45f23ce8e7062138f134185e71ce9a3843bb58a7
Emmy Steven <hello@emmysteven.com>
2021-06-30 14:42:25 +0100

    add method for adding members

--- client/src/app/core/services/member.service.ts
+++ client/src/app/core/services/member.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Router} from '@angular/router'
 import {HttpClient} from '@angular/common/http'
 import {Observable} from 'rxjs'
+import {User} from '@app/core/models/user'
 
 
 const baseUrl = 'http://localhost:3000/api/member';
@@ -15,6 +16,10 @@ export class MemberService {
 
   getAll(): Observable<any> {
     return this.http.get(baseUrl)
+  }
+
+  add(member: User) {
+    return this.http.post(`${baseUrl}auth/signup`, member);
   }
 
 }
