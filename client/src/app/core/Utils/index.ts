62a0edf54fa0284c225d75702c95114c1388c5f9
Emmy Steven <hello@emmysteven.com>
2021-06-30 15:05:12 +0100

    perf: add utility class

---
+++ client/src/app/core/Utils/index.ts
@@ -0,0 +1,16 @@
+export class Util {
+  static cells() {
+    return [
+      'Surulere',
+      'Ajah',
+      'Ajah 2',
+      'Ipaja/Egbeda',
+      'Oko Oba',
+      'Ogba',
+      'Omole',
+      'Ajao Estate',
+      'Gbagada',
+      'Magodo'
+    ]
+  }
+}
