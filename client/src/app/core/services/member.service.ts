c002cb08f9369f00bb2d9244af383a8ad878ae82
Emmy Steven <hello@emmysteven.com>
2021-07-05 16:25:13 +0100

    perf: change base url to a constant

--- client/src/app/core/services/member.service.ts
+++ client/src/app/core/services/member.service.ts
@@ -3,9 +3,10 @@ import {Router} from '@angular/router'
 import {HttpClient} from '@angular/common/http'
 import {Observable} from 'rxjs'
 import {User} from '@app/core/models/user'
+import { environment } from "@env/environment";
 
-
-const baseUrl = 'http://localhost:3000/api/member';
+// const httpOptions = { withCredentials: true };
+const BASE_URL = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,11 @@ export class MemberService {
   constructor(private router: Router, private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(baseUrl)
+    return this.http.get(`${BASE_URL}api/member`)
   }
 
   add(member: User) {
-    return this.http.post(`${baseUrl}auth/signup`, member);
+    return this.http.post(`${BASE_URL}api/member`, member);
   }
 
 }
