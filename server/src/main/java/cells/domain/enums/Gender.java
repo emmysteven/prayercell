da51a61c83bc7d2663199f0159051b044014266d
Emmy Steven <hello@emmysteven.com>
2021-07-05 16:26:32 +0100

    perf: add enum for gender

---
+++ server/src/main/java/cells/domain/enums/Gender.java
@@ -0,0 +1,11 @@
+package cells.domain.enums;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum Gender {
+    @JsonProperty("Male")
+    MALE,
+
+    @JsonProperty("Female")
+    FEMALE
+}
