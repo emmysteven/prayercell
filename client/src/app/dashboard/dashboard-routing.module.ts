8169a77c0e041ffd4c5ddeefb4bd44627492e294
Emmy Steven <hello@emmysteven.com>
2021-07-21 13:25:11 +0100

    refactor: rename import for UpdateMemberComponent

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '@app/dashboard/layout.component'
 import {
-  addMemberComponent,
   ListMemberComponent,
-  UpdateMemberComponent
+  AddEditMemberComponent
 } from '@app/dashboard/member'
 
 const routes: Routes = [
@@ -12,8 +11,8 @@ const routes: Routes = [
     path: '', component: LayoutComponent,
     children: [
       { path: 'member', component: ListMemberComponent },
-      { path: 'member/add', component: addMemberComponent },
-      { path: 'member/update/:id', component: UpdateMemberComponent },
+      { path: 'member/add', component: AddEditMemberComponent },
+      { path: 'member/edit/:id', component: AddEditMemberComponent },
     ]
   }
 ];
