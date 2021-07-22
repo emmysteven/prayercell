bd5c3213e36cd966cfc2a29469d54a9eac360517
Emmy Steven <hello@emmysteven.com>
2021-07-22 14:42:05 +0100

    rename update component to AddEditAdmin component

--- client/src/app/dashboard/admin/index.ts
+++ client/src/app/dashboard/admin/index.ts
@@ -1,4 +1,4 @@
 export * from './AddAdmin.component'
 export * from './ListAdmin.component'
-export * from './UpdateAdmin.component'
+export * from './AddEditAdmin.component'
 export * from './DeleteAdmin.component'
