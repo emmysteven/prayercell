78a52016e0cf7e07c4e0c97618a77251adcb19fd
Emmy Steven <hello@emmysteven.com>
2021-07-22 13:31:39 +0100

    add alert component to shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent, FooterComponent } from './index';
-import {AuthRoutingModule} from '@app/auth/auth-routing.module'
+import { AuthRoutingModule } from '@app/auth/auth-routing.module';
+import { HeaderComponent, FooterComponent, AlertComponent } from './index';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, AlertComponent],
   imports: [CommonModule, AuthRoutingModule],
-  exports: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent, AlertComponent],
 })
 export class SharedModule { }
