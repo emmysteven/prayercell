2bf4fa52c83ef6fa8c5b89e41c3f63e20112ce0b
Emmy Steven <hello@emmysteven.com>
2021-08-07 23:59:40 +0100

    import generic modal and add to declarations

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from '@app/auth/auth-routing.module';
 import { HeaderComponent, FooterComponent, AlertComponent } from './index';
+import { ModalComponent } from './components/modal.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, AlertComponent],
+  declarations: [HeaderComponent, FooterComponent, AlertComponent, ModalComponent],
   imports: [CommonModule, AuthRoutingModule],
   exports: [FooterComponent, HeaderComponent, AlertComponent],
 })
