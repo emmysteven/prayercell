80168c45e718ec71085a16caf0736834e504fae0
Emmy Steven <hello@emmysteven.com>
2021-08-08 01:02:13 +0100

    refactor: rename selector

--- client/src/app/shared/components/modal.component.ts
+++ client/src/app/shared/components/modal.component.ts
@@ -5,15 +5,55 @@ import { ModalService } from '@app/core/services'
 @Component({
   selector: 'app-modal',
   template: `
-    <p>
-      modal works!
-    </p>
+    <div class="app-modal">
+      <div class="app-modal-body">
+        <ng-content></ng-content>
+      </div>
+    </div>
+    <div class="app-modal-background"></div>
   `,
   styles: [
+    `app-modal {
+        display: none;
+      }
+
+      app-modal .app-modal {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 1000;
+        overflow: auto;
+      }
+
+      app-modal .app-modal .app-modal-body {
+        padding: 20px;
+        background: #fff;
+        margin: 40px;
+      }
+
+      app-modal .app-modal-background {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: #000;
+        opacity: 0.75;
+        z-index: 900;
+      }
+
+      body.app-modal-open {
+        overflow: hidden;
+      }
+    `
   ]
 })
-export class ModalComponent implements OnInit {
 
+
+export class ModalComponent implements OnInit {
+  
   @Input() id: string = '';
   private readonly element: any;
 
@@ -32,11 +72,11 @@ export class ModalComponent implements OnInit {
     document.body.appendChild(this.element);
 
     // close modal on background click
-    // this.element.addEventListener('click', el => {
-    //   if (el.target.className === 'jw-modal') {
-    //     this.close();
-    //   }
-    // });
+    this.element.addEventListener('click', (el: any) => {
+      if (el.target.className === 'app-modal') {
+        this.close();
+      }
+    });
 
     // add self (this modal instance) to the modal service so it's accessible from controllers
     this.modalService.add(this);
@@ -51,13 +91,13 @@ export class ModalComponent implements OnInit {
   // open modal
   open(): void {
     this.element.style.display = 'block';
-    document.body.classList.add('jw-modal-open');
+    document.body.classList.add('app-modal-open');
   }
 
   // close modal
   close(): void {
     this.element.style.display = 'none';
-    document.body.classList.remove('jw-modal-open');
+    document.body.classList.remove('app-modal-open');
   }
 
 }
