0c419ce02a83baee0da8c32480aa43ba0491b31e
Emmy Steven <hello@emmysteven.com>
2021-08-08 02:15:58 +0100

    uninstall material-ui

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,23 +9,22 @@ import { SharedModule } from "@app/shared";
 import { AuthModule } from '@app/auth';
 import { appInitializer } from '@app/core'
 import { AuthService } from '@app/core/services'
-import { ErrorInterceptor, JwtInterceptor } from '@app/core/interceptors';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module'
+import { ErrorInterceptor, JwtInterceptor } from '@app/core/interceptors';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    HttpClientModule,
-    AppRoutingModule,
-    DashboardModule,
-    BrowserModule,
-    SharedModule,
     AuthModule,
-    BrowserAnimationsModule,
-    MaterialModule
+    SharedModule,
+    BrowserModule,
+    DashboardModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
+
   providers: [
     { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AuthService] },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
