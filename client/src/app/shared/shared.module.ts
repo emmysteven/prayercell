8cc80c669f4f016e3b1250df5752665147c19371
Emmy Steven <hello@emmysteven.com>
2021-08-08 01:04:07 +0100

    refactor: optimize imports

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from '@app/auth/auth-routing.module';
-import { HeaderComponent, FooterComponent, AlertComponent } from './index';
-import { ModalComponent } from './components/modal.component';
+import { HeaderComponent, FooterComponent, AlertComponent, ModalComponent } from './index';
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, AlertComponent, ModalComponent],
   imports: [CommonModule, AuthRoutingModule],
-  exports: [FooterComponent, HeaderComponent, AlertComponent],
+  exports: [FooterComponent, HeaderComponent, AlertComponent, ModalComponent],
 })
 export class SharedModule { }
