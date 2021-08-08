e66182ff9ee3bc42b4e261af068349dd52f6f9cc
Emmy Steven <hello@emmysteven.com>
2021-08-08 01:03:02 +0100

    index modal component

--- client/src/app/shared/index.ts
+++ client/src/app/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './components/footer.component';
 export * from './components/header.component';
 export * from './components/alert.component';
+export * from './components/modal.component';
 export * from './shared.module';
