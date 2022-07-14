8381fe955ee9bae3289a68c40c830aa1cd6db51e
Emmy <hello@emmysteven.com>
2022-07-14 15:43:57 +0100

    fix: remove unnecessary whitespace
    fix #68

--- server/src/main/java/cells/application/payload/response/JwtAuthenticationResponse.java
+++ server/src/main/java/cells/application/payload/response/JwtAuthenticationResponse.java
@@ -3,11 +3,8 @@ package cells.application.payload.response;
 public class JwtAuthenticationResponse {
 
     private String accessToken;
-
     private String refreshToken;
-
     private String tokenType;
-
     private Long expiryDuration;
 
     public JwtAuthenticationResponse(String accessToken, String refreshToken, Long expiryDuration) {
@@ -20,7 +17,6 @@ public class JwtAuthenticationResponse {
     public String getAccessToken() {
         return accessToken;
     }
-
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
     }
@@ -28,15 +24,13 @@ public class JwtAuthenticationResponse {
     public String getTokenType() {
         return tokenType;
     }
-
     public void setTokenType(String tokenType) {
         this.tokenType = tokenType;
     }
-
+    
     public String getRefreshToken() {
         return refreshToken;
     }
-
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
@@ -44,7 +38,6 @@ public class JwtAuthenticationResponse {
     public Long getExpiryDuration() {
         return expiryDuration;
     }
-
     public void setExpiryDuration(Long expiryDuration) {
         this.expiryDuration = expiryDuration;
     }
