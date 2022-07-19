75f63863ccf9ce98564be22ce4bab7c8b10afd04
Emmy <hello@emmysteven.com>
2022-07-20 00:09:40 +0100

    refactor: add @Repository annotation to interface

--- server/src/main/java/cells/infrastructure/repository/RoleRepository.java
+++ server/src/main/java/cells/infrastructure/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package cells.infrastructure.repository;
 
 import cells.domain.entity.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
 }
