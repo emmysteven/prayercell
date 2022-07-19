7bd9c2a16efe640666568087926ed08810e06042
Emmy <hello@emmysteven.com>
2022-07-20 00:11:10 +0100

    refactor: add @Repository annotation to interface

--- server/src/main/java/cells/infrastructure/repository/UserRepository.java
+++ server/src/main/java/cells/infrastructure/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package cells.infrastructure.repository;
 
+import cells.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-import cells.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
