f9281e7c2c40efde6579f95ca8e5dc0a1909da93
Emmy <hello@emmysteven.com>
2022-07-20 00:10:05 +0100

    refactor: add @Repository annotation to interface

--- server/src/main/java/cells/infrastructure/repository/EmailVerificationTokenRepository.java
+++ server/src/main/java/cells/infrastructure/repository/EmailVerificationTokenRepository.java
@@ -2,9 +2,11 @@ package cells.infrastructure.repository;
 
 import cells.domain.entity.token.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
 
     Optional<EmailVerificationToken> findByToken(String token);
