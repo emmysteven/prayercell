d472aa8181ce6d39d33a2ea6836c77fd8a6c0cb6
Emmy <hello@emmysteven.com>
2022-07-20 00:08:44 +0100

    refactor: add @Repository annotation to interface

--- server/src/main/java/cells/infrastructure/repository/MemberRepository.java
+++ server/src/main/java/cells/infrastructure/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package cells.infrastructure.repository;
 import cells.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m FROM Member m WHERE m.id = ?1")
